3043f3c93922ad3e53e48e15a289d96a135e0d62
shani barak <shanibrk@gmail.com>
2022-06-06 12:23:00 +0300

    adding gluten free component

--- src/components/Gluten.jsx
+++ src/components/Gluten.jsx
@@ -1,9 +1,101 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import styled from 'styled-components'
+import { Splide, SplideSlide, splideSlide } from '@splidejs/react-splide'
+import '@splidejs/splide/dist/css/splide.min.css'
 
 const Gluten = () => {
+
+  const [gluten, setGluten]=useState([])
+
+  useEffect(()=>{
+    getGluten()
+  },[])
+
+  const getGluten=async()=>{
+    const check=localStorage.getItem('gluten');
+
+    if(check){
+        setGluten(JSON.parse(check))
+    }else{
+      const api= await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_KEY}&number=18&intolerances=gluten`)
+      const data= await api.json()
+      localStorage.setItem('gluten', JSON.stringify(data.recipes))
+      setGluten(data.recipes)
+      console.log(data.recipes)
+    }
+
+   
+  }
   return (
-    <div>Gluten</div>
+    <div>
+      
+        <Wrapper>
+          <h3>Gluten Free Picks</h3>
+          <Splide options={{
+            perPage:5,
+            arrows:false,
+            pagination:false,
+            drag:"free",
+            gap:"3rem"
+          }}>
+
+            {gluten.map((recipe)=>{
+              return(
+                <SplideSlide key={recipe.id}>
+                  <Card>
+                    <p>{recipe.title}</p>
+                    <img src={recipe.image} alt={recipe.title} />
+                    <Gradient/>
+                  </Card>
+                </SplideSlide>
+              )
+            })}
+          </Splide>
+        </Wrapper>
+      
+    </div>
   )
 }
+
+const Wrapper= styled.div`
+margin: 4rem 0rem
+`
+const Card= styled.div`
+min-height:20rem;
+border-radius:1rem;
+overflow:hidden;
+position:relative
+
+img{
+  border-radius:2rem;
+  posu=ituin:absolute,
+  left:0,
+  width:100%;
+  objext-fit:cover;
+}
+p{
+  position:absolute;
+  z-index:10;
+  left:50%;
+  bottom:0%;
+  transform:translate(-50%, 0%);
+  color:white;
+  width:100%;
+  text-align:center;
+  font-weight:600;
+  font-size:1rem;
+  height:40%;
+  display:flex;
+  justify-content:center;
+  align-items:center;
+}
+`
+const Gradient=styled.div`
+z-index:3;
+position:absolute;
+width:100%;
+height:100%;
+background: linear-gradient(rgba(0,0,0,0), rgba(0,0,0,0.5));
+`
 
 export default Gluten
--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
-import { Splide, splideSlide } from '@splidejs/react-splide'
+import { Splide, SplideSlide, splideSlide } from '@splidejs/react-splide'
 import '@splidejs/splide/dist/css/splide.min.css'
 
 const Popular = () => {
@@ -12,29 +12,48 @@ const Popular = () => {
   },[])
 
   const getPopular=async()=>{
-    const api= await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=18`)
-    const data= await api.json()
-    setPopular(data.recipes)
+    const check=localStorage.getItem('popular');
+
+    if(check){
+      setPopular(JSON.parse(check))
+    }else{
+      const api= await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_KEY}&number=18`)
+      const data= await api.json()
+      localStorage.setItem('popular', JSON.stringify(data.recipes))
+      setPopular(data.recipes)
+      console.log(data.recipes)
+    }
+
+   
   }
   return (
-    <div>{popular.map((recipe)=>{
-      return(
+    <div>
+      
         <Wrapper>
           <h3>Popular</h3>
-          <Splide>
+          <Splide options={{
+            perPage:4,
+            arrows:false,
+            pagination:false,
+            drag:"free",
+            gap:"5rem"
+          }}>
 
             {popular.map((recipe)=>{
               return(
-                <Card>
-                  <p>{recipe.title}</p>
-                  <img src={recipe.image} alt={recipe.title} />
-                </Card>
+                <SplideSlide key={recipe.id}>
+                  <Card>
+                    <p>{recipe.title}</p>
+                    <img src={recipe.image} alt={recipe.title} />
+                    <Gradient/>
+                  </Card>
+                </SplideSlide>
               )
             })}
           </Splide>
         </Wrapper>
-      )
-    })}</div>
+      
+    </div>
   )
 }
 
@@ -45,10 +64,38 @@ const Card= styled.div`
 min-height:25rem;
 border-radius:2rem;
 overflow:hidden;
+position:relative
 
 img{
-  border-radius:2rem
+  border-radius:2rem;
+  posu=ituin:absolute,
+  left:0,
+  width:100%;
+  objext-fit:cover;
 }
+p{
+  position:absolute;
+  z-index:10;
+  left:50%;
+  bottom:0%;
+  transform:translate(-50%, 0%);
+  color:white;
+  width:100%;
+  text-align:center;
+  font-weight:600;
+  font-size:1rem;
+  height:40%;
+  display:flex;
+  justify-content:center;
+  align-items:center;
+}
+`
+const Gradient=styled.div`
+z-index:3;
+position:absolute;
+width:100%;
+height:100%;
+background: linear-gradient(rgba(0,0,0,0), rgba(0,0,0,0.5));
 `
 
 export default Popular
